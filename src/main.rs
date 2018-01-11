f1fcacf1f243b6f96f7ccf5d06e9f4a8258d7867
Ta Thanh Dinh <tathanhdinh@gmail.com>
2018-01-11 01:47:52 +0100

    create GUI

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ use nwg::{Event, Ui, FileDialog, simple_message, fatal_message, dispatch_events}
 #[derive(Debug, Clone, Hash)]
 pub enum AppId {
     // Controls
-    MainWindow
+    MainWindow,
+    PeSectionGroupBox,
+    FileNameLabel,
+
+    // Resources
+    GroupBoxFont
 }
 
 // use AppId::*;
@@ -14,11 +19,46 @@ pub enum AppId {
 nwg_template!(
     head: setup_ui<AppId>,
     controls: [
-        (AppId::MainWindow, nwg_window!(title = "PeC - PE Characteristics"; size = (400, 247)))
+        (AppId::MainWindow, 
+         nwg_window!(title = "PeC - PE Characteristics"; 
+                     position=(100, 100); 
+                     size=(300, 200))),
+
+        (AppId::FileNameLabel, 
+         nwg_label!(parent = AppId::MainWindow;
+                    text = "Input PE"; 
+                    position = (20, 30); 
+                    size = (100, 30); 
+                    font = Some(AppId::GroupBoxFont))),     
+                    
+        (AppId::PeSectionGroupBox,
+         nwg_groupbox!(parent = AppId::MainWindow;
+                       text = "Sections";
+                       position = (20, 80); 
+                       size = (250, 80); 
+                       font=Some(AppId::GroupBoxFont)))
+
+        // (AppId::MainWindow, 
+        //  nwg_window!(title = "Nwg Showcase"; 
+        //              position = (100, 100); 
+        //              size = (500, 400))),
+
+        // (AppId::FileNameLabel, 
+        //  nwg_label!(parent = AppId::MainWindow; 
+        //             text = "Selected PE"; 
+        //             position = (120, 90); 
+        //             size=(200, 25))),
+
+        // (AppId::PeSectionGroupBox, 
+        //  nwg_groupbox!(parent=AppId::MainWindow; 
+        //                text="Section"; 
+        //                position=(20, 40); 
+        //                size=(130, 80)))
     ];
     events: [
     ];
     resources: [
+        (AppId::GroupBoxFont, nwg_font!(family="Segoe UI"; size=14))
     ];
     values: []
 );
